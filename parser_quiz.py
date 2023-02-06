78dbe0e926e7fe0cd45c3bd85c50b2410a110fb7
Tim Sarkisiani <timsarkisiani@gmail.com>
2023-02-07 01:38:30 +0400

    Update

--- parser_quiz.py
+++ parser_quiz.py
@@ -1,10 +1,8 @@
-# ვაიმპორტებ საჭირო მოდულებს
 import requests
 from bs4 import BeautifulSoup
 import csv
 
 
-# აქ შემოვიღე კონსტანტები
 URL = 'https://xiaomi.com.ge/product-category/smart-device/'
 HEADERS = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36', 'accept': '*/*'}
 FILE = 'products.csv'
